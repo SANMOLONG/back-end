177f873caf7a09639897c0404ade55ed2a4dca7f
Mi <mi_aa_@naver.com>
2023-09-15 17:26:06 +0900

    first commit

---
+++ src/main/java/com/innocamp/sanmolong/SanmolongApplication.java
@@ -0,0 +1,13 @@
+package com.innocamp.sanmolong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanmolongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanmolongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innocamp/sanmolong/SanmolongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innocamp.sanmolong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanmolongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
