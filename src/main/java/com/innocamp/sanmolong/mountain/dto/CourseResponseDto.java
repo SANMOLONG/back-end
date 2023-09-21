a67a38e28da3c08b8a68bc12e4b78ce3b1199269
thing-zoo <cathy4025@gmail.com>
2023-09-21 21:10:06 +0900

    feat: 산별/코스별 정보 조회

---
+++ src/main/java/com/innocamp/sanmolong/mountain/dto/CourseResponseDto.java
@@ -0,0 +1,19 @@
+package com.innocamp.sanmolong.mountain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CourseResponseDto {
+    String mountain;
+    String course;
+    String level;
+    String spendTime;
+    String courseImg;
+    String coursePhone;
+    String departNM;
+    String departAD;
+    String courseDetail;
+    String coursePic;
+}
