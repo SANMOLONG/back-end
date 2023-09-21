d42c901581465a5a7df4e366a50181bdfb5b0dc9
thing-zoo <cathy4025@gmail.com>
2023-09-22 00:51:51 +0900

    feat: 게시글 조회 시 반환 정보 수정

--- src/main/java/com/innocamp/sanmolong/comment/dto/CommentResponseDto.java
+++ src/main/java/com/innocamp/sanmolong/comment/dto/CommentResponseDto.java
@@ -3,16 +3,19 @@ package com.innocamp.sanmolong.comment.dto;
 import com.innocamp.sanmolong.comment.entity.Comment;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class CommentResponseDto {
     private Long commentId;
     private String nickname;
     private String content;
+    private LocalDate createdAt;
 
     public CommentResponseDto(Comment comment) {
         this.commentId = comment.getId();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
-
+        this.createdAt = comment.getCreatedAt();
     }
 }
