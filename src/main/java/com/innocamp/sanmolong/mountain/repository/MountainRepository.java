b3be1ffe1ff32680be6a991fd85bb77895993356
thing-zoo <cathy4025@gmail.com>
2023-09-22 05:57:11 +0900

    fix: 코스 중복 오류 수정

--- src/main/java/com/innocamp/sanmolong/mountain/repository/MountainRepository.java
+++ src/main/java/com/innocamp/sanmolong/mountain/repository/MountainRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MountainRepository extends JpaRepository<Mountain, Long> {
+    @Query("select m from Mountain m where m.mountain = ?1 and m.course = ?2 group by m.mountain, m.course")
     Optional<Mountain> findByMountainAndCourse(String mountain, String course);
 
     Optional<Mountain> findByMountainAndCourseAndDepartNm(String mountain, String course, String departNm);
